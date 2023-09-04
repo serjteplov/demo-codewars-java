cf1c259cdda4519ecee77396b95ed851b8f94ff2
Sergey Teplov <serj.teplov>
2023-09-04 15:36:04 +0300

    yandex fintech night - samokaty

--- src/main/java/ru/serj/Main.java
+++ src/main/java/ru/serj/Main.java
@@ -1,39 +1,49 @@
 package ru.serj;
 
-import java.math.BigInteger;
-
-import static java.math.BigInteger.ONE;
-import static java.math.BigInteger.ZERO;
-
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 3;
-        int w = 2*n-1;
 
-        BigInteger[][] matrix = new BigInteger[w+2][w+1];
-        for (int j = 0; j <= w-1; j++) {
-            for (int i = 1; i <= w - Math.abs(n-1 - j); i++) {
-                matrix[i][j] = ZERO;
-            }
-        }
-        matrix[n][w] = ONE;
+        String input1 = "[9, 9, 4, 8, 9, 9]";
+        String input2 = "[9, 9, 6]";
 
-        for (int j = w-1; j >= 0; j--) {
-            for (int i = w; i > 0; i--) {
-                if (matrix[i][j] == null) continue;
-                int adjust = j < n - 1 ? 1 : 0;
-                var rightUp = matrix[i-1+adjust][j+1] == null ? ZERO : matrix[i-1+adjust][j+1];
-                var rightDown = matrix[i+adjust][j+1] == null ? ZERO : matrix[i+adjust][j+1];
-                var down = matrix[i+1][j] == null ? ZERO : matrix[i+1][j];
-                matrix[i][j] = rightUp.add(rightDown).add(down);
-            }
+        input1 = input1.substring(1, input1.length() - 1);
+        input2 = input2.substring(1, input2.length() - 1);
+        int[] arr1 = Stream.of(input1.split(", ")).mapToInt(Integer::parseInt).toArray();
+        int[] arr2 = Stream.of(input2.split(", ")).mapToInt(Integer::parseInt).toArray();
+        int l1 = arr1.length;
+        int l2 = arr2.length;
+        int[] arr3 = new int[Math.max(l1, l2) + 1];
+        int l3 = arr3.length;
+
+        for (int i = l3 - 2; i >= 0; i--) {
+            int one = i > l1 - 1 ? 0 : arr1[i];
+            int two = i > l2 - 1 ? 0 : arr2[i];
+            arr3[l3 - 2 - i] = one + two;
         }
 
+        int vume = 0;
+        for (int i = 0; i <= l3 - 1; i++) {
+            arr3[i] += vume;
+            vume = arr3[i] / 10;
+            if (vume > 0) {
+                arr3[i] = arr3[i] % 10;
+            }
+        }
+        if (arr3[l3 - 1] == 0) {
+            arr3 = Arrays.copyOf(arr3, l3 - 1);
+            l3--;
+        }
+        for (int i = 0; i <= (l3 / 2) - 1; i++) {
+            arr3[i] += arr3[l3 - 1 - i];
+            arr3[l3 - 1 - i] = arr3[i] - arr3[l3 - 1 - i];
+            arr3[i] = arr3[i] - arr3[l3 - 1 - i];
+        }
 
-
-        System.out.println("bee = " + matrix[1][0]);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr3));
     }
-
-
 }
