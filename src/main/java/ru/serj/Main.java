5cb1227971a636cd8cb2f7d68e73532a1e6bcc6b
Sergey Teplov <serj.teplov>
2023-10-30 07:07:01 +0300

    read big file

--- src/main/java/ru/serj/Main.java
+++ src/main/java/ru/serj/Main.java
@@ -1,62 +1,31 @@
 package ru.serj;
 
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static final int POLOSA = 3;
-    private static final int TARGET = 6;
 
     public static void main(String[] args) {
-        boolean[][] obst = new boolean[TARGET+1][POLOSA+2];
-        for (int i = 0; i <= TARGET; i++) {
-            obst[i][0] = true;
-            obst[i][POLOSA+1] = true;
-        }
-        obst[1][1] = true;
-        obst[2][1] = true;
-        obst[2][2] = true;
-        obst[3][2] = true;
-        obst[3][3] = true;
-        obst[4][1] = true;
-        obst[4][2] = true;
-        obst[4][3] = true;
-        obst[5][1] = true;
-        obst[5][2] = true;
+        try (InputStream inputStream = Main.class.getResourceAsStream("/1.txt");
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        ) {
 
-        for (int i = 0; i <= POLOSA+1; i++) System.out.print("  " + i + "    ");
-        System.out.println();
-        for (int i = TARGET; i >= 0; i--) System.out.println(i + Arrays.toString(obst[i]));
-
-        System.out.println("recursive = " + recur(obst, 0, 2));
-        boolean[] firstCheck = new boolean[]{false, true, true, true, false};
-        System.out.println("iterative = " + iterat(obst, firstCheck));
-    }
-
-    public static int recur(boolean[][] obst, int i, int j) {
-        if (i == TARGET) return i;
-        int max1 = !obst[i+1][j-1]                                    ? recur(obst, i+1, j-1) : 0;
-        int max2 = !obst[i+1][j]    && max1 < TARGET                  ? recur(obst, i+1, j)     : 0;
-        int max3 = !obst[i+1][j+1]  && max1 < TARGET && max2 < TARGET ? recur(obst, i+1, j+1) : 0;
-        int tsarMax = Math.max(max1, Math.max(max2, max3));
-        return tsarMax == 0 ? i : tsarMax;
-    }
-
-    public static int iterat(boolean[][] obst, boolean[] lineToCheck) {
-        for (int i = 0; i < obst.length-1; i++) {
-
-            boolean[] temp = new boolean[5];
-            for (int k = 1; k <= POLOSA; k++) {
-                if (lineToCheck[k] && !obst[i + 1][k]) {
-                    temp[k] = true;
-                    temp[k+1] = true;
-                    temp[k-1] = true;
-                }
+            File file = new File("src/main/resources/1.txt");
+            char[] buf = new char[1000];
+            int i = 0;
+            while (i+31 < file.length()) {
+                if (i+31 < file.length()) bufferedReader.read(buf, i, i + 30);
+                i += 33;
+                System.out.println("buf = " + new String(buf));
             }
-
-            if (!temp[1] && !temp[2] && !temp[3]) return i;
-            lineToCheck = Arrays.copyOf(temp, POLOSA + 2);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return obst.length-1;
+
+
     }
 }
